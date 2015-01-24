6f72afe30dc37781a10a60d3c97a99e811f782f2
Ellis Jackson <eabjackson@hotmail.co.uk>
2015-01-24 04:54:41 +0000

    added the outcome strings

--- AR/Assets/Scripts/Question Boxes/Questions.cs
+++ AR/Assets/Scripts/Question Boxes/Questions.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Questions {
 	
 	protected string[] QuestionArr; 
-	protected string[,] AnswerArr; 
+	protected string[,] AnswerArr;
+	protected string[,] OutcomeArr; 
 	public int numQuestions; 
+	public List<int> ListaskedQ= new List<int>();
 
 
 	public Questions()
@@ -34,7 +37,7 @@ public class Questions {
 		QuestionArr [19] = "Elder, your mother has invited you over for dinner, what do we do now?";
 		QuestionArr [20] = "we have encountered a pack of wolves elder, what do we do now?";
 		QuestionArr [21] = "elder, the local farmer Gunter, his chicken has laid a couple of eggs, what do we do now?"; 
-		QuestionArr [22] = "One of the local villager wants to become artist, what do we do now?"; 
+		QuestionArr [22] = "One of the local villager wants to become an artist, what do we do now?"; 
 		QuestionArr [23] = "The harvest looks plentiful already, what should we do now?";
 		QuestionArr [24] = "Elder, this villager is annoyed at the tree near his hut, what do we do now?";
 		QuestionArr [25] = "The crops haven’t grown elder, what do we do now?"; 
@@ -57,10 +60,8 @@ public class Questions {
 		AnswerArr = new string[41, 2];
 		AnswerArr [0, 0] = "Destroy the food,it will only cause harm!";
 		AnswerArr [0, 1] = "Keep the food, I'm sure we'll be fine!";
-
 		AnswerArr [1, 0] = "Stop drinking from the river, and tighten up the supplies.";
 		AnswerArr [1, 1] = "Keep the supply up, we need the water.";
-
 		AnswerArr [2, 0] = "Of course I will help you, here take some supplies with you, I hope to see you again.";
 		AnswerArr [2, 1] = "Who do you think you are! We need our supplies!";
 		//answers to the good outcome of people returning
@@ -69,115 +70,166 @@ public class Questions {
 		//answers to the bad outcome of people returning
 		AnswerArr [4, 0] = "...";
 		AnswerArr [4, 1] = "...";
-
 		AnswerArr [5, 0] = "Use them to produce weapons for the hunters! ";
 		AnswerArr [5, 1] = "I could use some new valuables give them to me! ";
-
 		AnswerArr [6, 0] = "Okay mighty gods I will do as you ask.";
 		AnswerArr [6, 1] = "Im the one with the power hear, I do not bend to your will!";
-
 		AnswerArr [7, 0] = "Throw them on the fire, our people could do with some warmth.";
 		AnswerArr [7, 1] = "Use them to build more huts, our people would do with some shelter. ";
-
 		AnswerArr [8, 0] = "Prepare a mighty feast let no person go unfed.";
 		AnswerArr [8, 1] = "Put them in the storage with the rest, we must be careful with supplies.";
-
 		AnswerArr [9, 0] = "Its fine I’m sure you tried your best. ";
 		AnswerArr [9, 1] = "Your incompetence cannot be forgiven, of with your head!";
-
 		AnswerArr [10, 0] = "Make a sacrifice to please the rain god.";
 		AnswerArr [10, 1] = "Do nothing it will rain soon.";
-
 		AnswerArr [11, 0] = "Kill ill people, save the rest of us.";
 		AnswerArr [11, 1] = "Help them as much as you can.";
-
 		AnswerArr [12, 0] = "Oh s*$t!";
 		AnswerArr [12, 1] = "Oh f#%k!";
-
 		AnswerArr [13, 0] = "You are foolish if you think you can best me in a fight.";
 		AnswerArr [13, 1] = "You are smart enough to challenge me, you are smart enough to win the fight";
-
 		AnswerArr [14, 0] = "Bring me the finest mate, I need an heir.";
 		AnswerArr [14, 1] = "I am still young yet.";
-
 		AnswerArr [15, 0] = "Invite them to join us.";
 		AnswerArr [15, 1] = "Don’t allow them in they must leave.";
-
 		AnswerArr [16, 0] = "Punish him, this crime cannot go unpunished!";
 		AnswerArr [16, 1] = "Leave him be, we can let him off, this time.";
-
 		AnswerArr [17, 0] = "This claim is ridiculous banish him for his incompetence.";
 		AnswerArr [17, 1] = "I cant say for sure if he is or not, but I shall adopt him for the mena time.";
-
 		AnswerArr [18, 0] = "It takes time to be a good hunter, let him be.";
 		AnswerArr [18, 1] = "Train him with the other hunters, he can learn.";
-
 		AnswerArr [19, 0] = "I cant possibly go and see her I have a village to run.";
 		AnswerArr [19, 1] = "Okay lets go, but pack some supplies for the trip.";
-
 		AnswerArr [20, 0] = "we can train them and use them to aid our hunting!";
 		AnswerArr [20, 1] = "just kill them we could do with the extra food.";
-
 		AnswerArr [21, 0] = "That’s my dinner sorted, bring them to me!";
 		AnswerArr [21, 1] = "Store them, every little helps!";
-
 		AnswerArr [22, 0] = "Artists aren’t helpful were not helping him!";
 		AnswerArr [22, 1] = "Supply him with a few canvas, maybe we can display his work!";
-
 		AnswerArr [23, 0] = "Harvest it now, we cant risk it with the weather!";
 		AnswerArr [23, 1] = "Wait a few days, it could get better!";
-
 		AnswerArr [24, 0] = "Cut the tree down it wont cause any harm.";
 		AnswerArr [24, 1] = "Leave the tree, im sure he can live with it. ";
-
 		AnswerArr [25, 0] = "Send out the hunters, see if they can gather some food. ";
 		AnswerArr [25, 1] = "Just be careful with the supplies we can make it through the week. ";
-
 		AnswerArr [26, 0] = "Its only a sweet roll this is no concern to me!";
 		AnswerArr [26, 1] = "Discipline him, I think an arrow to the knee should suffice!";
-
 		AnswerArr [27, 0] = "Build some farms, it might use some supplies now but it will be worth it in the long run. ";
 		AnswerArr [27, 1] = "We don’t need anymore farms, well be ok.";
-
 		AnswerArr [28, 0] = "Sure we can trade, let him in!";
 		AnswerArr [28, 1] = "No we don’t need anyone from the outside interfering with us, send him away!";
-
 		AnswerArr [29, 0] = "...,";
 		AnswerArr [29, 1] = "...";
-
 		AnswerArr [30, 0] = "...";
 		AnswerArr [30, 1] = "...";
-
 		AnswerArr [31, 0] = "That’s very invasive, don’t let him continue ";
 		AnswerArr [31, 1] = "Let him do it, it keeps up appearances ";
-
 		AnswerArr [32, 0] = "Sanction.";
 		AnswerArr [32, 1] = "Don’t ask.";
-
-		AnswerArr [33, 0] = "a.	Clean it up ";
-		AnswerArr [33, 1] = "b.	Leave it ";
-
-		AnswerArr [34, 0] = "a.	Cleanse the cattle ";
-		AnswerArr [34, 1] = "b.	Cleanse Dan.  ";
-
-		AnswerArr [35, 0] = "a.	These things happen from time to time, its fine.";
-		AnswerArr [35, 1] = "b.	This is unacceptable, to miss a hunt, he needs to be punished! ";
-
-		AnswerArr [36, 0] = "a.	Let him carry on, its not to severe.";
-		AnswerArr [36, 1] = "b.	This is unacceptable it cannot continue ";
-
-		AnswerArr [37, 0] = "a.	Fight him";
-		AnswerArr [37, 1] = "b.	Make him flee";
-
-		AnswerArr [38, 0] = "a.	Fight her";
-		AnswerArr [38, 1] = "b.	Make her flee";
-
+		AnswerArr [33, 0] = "Clean it up ";
+		AnswerArr [33, 1] = "Leave it ";
+		AnswerArr [34, 0] = "Cleanse the cattle ";
+		AnswerArr [34, 1] = "Cleanse Dan.  ";
+		AnswerArr [35, 0] = "These things happen from time to time, its fine.";
+		AnswerArr [35, 1] = "This is unacceptable, to miss a hunt, he needs to be punished! ";
+		AnswerArr [36, 0] = "Let him carry on, its not to severe.";
+		AnswerArr [36, 1] = "This is unacceptable it cannot continue ";
+		AnswerArr [37, 0] = "Fight him";
+		AnswerArr [37, 1] = "Make him flee";
+		AnswerArr [38, 0] = "Fight her";
+		AnswerArr [38, 1] = "Make her flee";
 		AnswerArr [39, 0] = "Fight him";
 		AnswerArr [39, 1] = "Make him flee";
-
 		AnswerArr [40, 0] = "Fight him.";
 		AnswerArr [40, 1] = "Make him flee.";
 		//numQuestions = QuestionArr.Length;
+
+
+		OutcomeArr = new string[41, 2];
+		OutcomeArr [0, 0] = "loseFood";
+		OutcomeArr [0, 1] = "loseHappiness, losePopulation";
+		OutcomeArr [1, 0] = "loseWater";
+		OutcomeArr [1, 1] = "loseHappiness, losePopulation";
+		OutcomeArr [2, 0] = "losePopulation, loseSupplies, gainHappiness";
+		OutcomeArr [2, 1] = "losePopulation, loseHappiness";
+		//answers to the good outcome of people returning
+		OutcomeArr [3, 0] = "gainSupplies";
+		OutcomeArr [3, 1] = "gainSupplies";
+		//answers to the bad outcome of people returning
+		OutcomeArr [4, 0] = "loseSupplies";
+		OutcomeArr [4, 1] = "loseSupplies";
+		OutcomeArr [5, 0] = "gainFoodIncrease";
+		OutcomeArr [5, 1] = "loseHappiness, goldHut";
+		OutcomeArr [6, 0] = "losePopulation, gainHappiness";
+		OutcomeArr [6, 1] = "loseHappiness";//natural desaster chance increase
+		OutcomeArr [7, 0] = "gainHapiness";
+		OutcomeArr [7, 1] = "gainPopulation";
+		OutcomeArr [8, 0] = "gainHappiness";
+		OutcomeArr [8, 1] = "gainFood";
+		OutcomeArr [9, 0] = "gainHappiness";
+		OutcomeArr [9, 1] = "losePopulation, loseHappiness, loseFoodIncrease";
+		OutcomeArr [10, 0] = "losePopulation, gainHappiness";
+		OutcomeArr [10, 1] = "loseHappiness, loseWater";
+		OutcomeArr [11, 0] = "losePopulation, loseHappiness";//long turm pop increas
+		OutcomeArr [11, 1] = "loseSupplies, gainHappiness";//long turm pop decrease
+		OutcomeArr [12, 0] = "loseSupplies, loseSupplies, losePopulation";
+		OutcomeArr [12, 1] = "loseRandom";
+		OutcomeArr [13, 0] = "lose1Population, gainHappiness";
+		OutcomeArr [13, 1] = "loseGame";
+		OutcomeArr [14, 0] = "randomHappiness";
+		OutcomeArr [14, 1] = "nothing";
+		OutcomeArr [15, 0] = "gainPopulation, gainHappiness, gainFoodIncrease";
+		OutcomeArr [15, 1] = "loseHappiness";
+		OutcomeArr [16, 0] = "lose1Population, gainHappiness";
+		OutcomeArr [16, 1] = "loseHappiness";
+		OutcomeArr [17, 0] = "lose1Population";
+		OutcomeArr [17, 1] = "loseHappiness";
+		OutcomeArr [18, 0] = "loseFoodIncrease";
+		OutcomeArr [18, 1] = "loseFood";
+		OutcomeArr [19, 0] = "loseHappiness";
+		OutcomeArr [19, 1] = "loseFood, gainHappiness";
+		OutcomeArr [20, 0] = "gainFoodIncrease";
+		OutcomeArr [20, 1] = "gainFood";
+		OutcomeArr [21, 0] = "gain1Happiness";
+		OutcomeArr [21, 1] = "gain1Food";
+		OutcomeArr [22, 0] = "gainGraffiti";
+		OutcomeArr [22, 1] = "gainFineArt";
+		OutcomeArr [23, 0] = "gainFood";
+		OutcomeArr [23, 1] = "randomFood";
+		OutcomeArr [24, 0] = "cutTree";
+		OutcomeArr [24, 1] = "nothing";
+		OutcomeArr [25, 0] = "gainFood";
+		OutcomeArr [25, 1] = "loseHappiness";
+		OutcomeArr [26, 0] = "loseHappiness";
+		OutcomeArr [26, 1] = "gainHappiness, arrowKnee";
+		OutcomeArr [27, 0] = "loseFood, gainFoodIncrease";
+		OutcomeArr [27, 1] = "nothing";
+		OutcomeArr [28, 0] = "gainSupplies";
+		OutcomeArr [28, 1] = "nothing";
+		OutcomeArr [29, 0] = "Q30";
+		OutcomeArr [29, 1] = "nothing";
+		OutcomeArr [30, 0] = "burntHut";
+		OutcomeArr [30, 1] = "burntHut";
+		OutcomeArr [31, 0] = "loseHappiness";
+		OutcomeArr [31, 1] = "gainHappiness";
+		OutcomeArr [32, 0] = "lose1happiness";
+		OutcomeArr [32, 1] = "gain1Population";
+		OutcomeArr [33, 0] = "loseHappiness";
+		OutcomeArr [33, 1] = "gainHappiness";
+		OutcomeArr [34, 0] = "loseFood";
+		OutcomeArr [34, 1] = "lose1Population";
+		OutcomeArr [35, 0] = "gain1Happiness";
+		OutcomeArr [35, 1] = "lose1Happiness";
+		OutcomeArr [36, 0] = "loseFood, gainPopulation";
+		OutcomeArr [36, 1] = "loseHappiness";
+		OutcomeArr [37, 0] = "lose1Food, gain1Population";
+		OutcomeArr [37, 1] = "nothing";
+		OutcomeArr [38, 0] = "lose1Food, gain1Population";
+		OutcomeArr [38, 1] = "nothing";
+		OutcomeArr [39, 0] = "lose1Food, gain1Population";
+		OutcomeArr [39, 1] = "nothing";
+		OutcomeArr [40, 0] = "lose1Food, gain1Population";
+		OutcomeArr [40, 1] = "nothing";
 	}
 
 	public string returnQuestion(int qID)
